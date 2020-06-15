99234ca8483205a2b33a7b2e0d0b33227dbd6f6f
mx61tt <56231649+mx61tt@users.noreply.github.com>
2020-06-15 15:11:11 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Updating MSF-EGGHUNTER for x64..."
+
+sudo cp msf-egghunter.rb /usr/share/metasploit-framework/tools/exploit/egghunter.rb
+sudo cp rex-exploitation-egghunter.rb /usr/share/metasploit-framework/vendor/bundle/ruby/2.7.0/gems/rex-exploitation-0.1.24/lib/rex/exploitation/egghunter.rb
+
+echo "MSF-EGGHUNTER updated."
+echo "Check: msf-egghunter --help"
